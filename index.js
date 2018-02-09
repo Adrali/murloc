1db49b2c5ad5246e236f5c6b24cc4bfa42424450
Adrali <36172525+Adrali@users.noreply.github.com>
2018-02-09 16:32:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
+var prefix = 'm.'
+
 
 bot.on('ready',function () {
     bot.user.setGame('Mrgrgl').catch(console.error)
 })
 
 bot.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.content === prefix + 'ping') {
       message.reply('pong !')
     }
-    if (message.content === 'tic') {
+    if (message.content === prefix + 'tic') {
         message.reply('tac !')
     }
     if (message.content === 'nyd') {
